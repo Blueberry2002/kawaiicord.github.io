c2798c7a99b109efffc0aaab6a8b0713f735b706
Blueberry2002 <136845889+Blueberry2002@users.noreply.github.com>
2023-09-08 10:58:03 -0800

    Delete frontend directory

--- frontend/src/components/utils/buttons/circle-button.tsx
+++
@@ -1,7 +0,0 @@
-const CircleButton: React.FunctionComponent<any> = (props) => {
-  return (
-    <button className="rounded-full ring ring-gray-400 secondary px-4 py-1">{props.children}</button>
-  );
-}
- 
-export default CircleButton;
--- frontend/src/environment.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  apiURL: 'http://localhost:3000/api/v1',
-  rootAPIURL: 'http://localhost:3000',
-};
